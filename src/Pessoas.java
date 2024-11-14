8d4f6110b1ba90f186e7b866b09918f5f03b26c3
Alexandre <abcampos2021@gmail.com>
2024-11-13 21:03:37 -0300

    first commit

---
+++ src/Pessoas.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+
+public class Pessoas {
+	String nome;
+	double altura;
+	LocalDate dataDeNascimento;
+	String email;
+	
+	public Pessoas (String nome, String email, double altura, LocaDate dataDeNascimento) {
+		this.nome = nome;
+		this.altura = altura;
+		
+		
+	}
+
+}
